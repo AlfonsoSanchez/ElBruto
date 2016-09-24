44fbd9fa63981e397f5a35aa2a8803589c3a0fb7
AlfonsoSanchez <alfonsosanchez1896@gmail.com>
2016-09-24 11:31:28 +0200

    Add more functions

--- Bruto/Bruto/main.cpp
+++ Bruto/Bruto/main.cpp
@@ -6,22 +6,42 @@ using namespace std;
 
 
 char name[20];
-char nameClan[20];
-char setName(char newname[20])
+char nameClan[20] = "Default";
+int disciples = 0, numdisciples = 0;
+
+void setName(char newname[20])
 {
+	cout << "You change : " << name << " for " << newname << endl;
+	
+}
+void printName()
+{
+	cout << "\nYour name are: "<< name;
+}
+void printNumDisciples()
+{
+	cout << disciples;
+}
+void setNumDisciples()
+{
+	 disciples += numdisciples;
+	 cout << "\nthe num of disciples are: " << disciples;
+}
+void printClanName()
+{
+	cout << "\n Clan "<< nameClan;
+}
+void setClanName(char newClaname[20])
+{
+	cout << "You change : " << nameClan << " for " << newClaname << endl;
 
-	if (!strcmp(newname, name)) {
-		cout << "You change :" << name << "for" << newname << endl;
-
-	}
-	else { cout << "Your new name is equal to the old"; }
-	return newname[20];
 }
 int main()
 {
 	bool quit = false;
 	unsigned int order;
-	char decision[20], newname[20];
+	char  newname[20], newClanName[20];
+	int decision;
 	cout << "Hello This is a mini tutorial, when i ask for you order, you can say:\n 0-Quit\n 1-Name\n 2-Num of disciplines\n 3-Clan name\n 4-Capabilities\n 5-Weapons\n 6-Animals\n 7-Registration to the next tournament\n";
 	cout << "Well, you enter the number, LET'S START!!!! \n";
 	cout << "Say your name: ";
@@ -40,63 +60,68 @@ int main()
 			quit = true;
 			break;
 		case 1:
-			cout << "\nYou want remember your name, change name or you have wrong? (say remember, change or wrong)";
+			cout << "\nYou want 1-remember your name, 2-change name or 3-you have wrong?";
 			cin >> decision;
-			if (strcmp(decision, "remember")) {  break; } // de aqui para abajo se organiza falta funciones;
-			else if (strcmp(decision, "change\n")) { 
+			if (decision == 1) { printName(); break; } // de aqui para abajo se organiza falta funciones;
+			else if (decision ==2) { 
 				cout << "\n Say the new name: ";
 				cin >> newname;
-				name[20] = setName(newname);
+				setName(newname);
+				strcpy_s(name, newname);
 				break; }
-			else if (strcmp(decision, "wrong")) { break; }
+			else if (decision ==3) { break; }
 			else { cout << "that's not are nothing!"; break; }
 
 		case 2:
-			cout << "\nYou want remember your discipline, change discipline or you have wrong? (say remember, change or wrong)";
+			cout << "\nYou want 1-remember your disciples, 2-change disciples or 3-you have wrong?";
 			cin >> decision;
-			if (strcmp(decision, "remember")) { break; }
-			else if (strcmp(decision, "change")) { break; }
-			else if (strcmp(decision, "wrong")) { break; }
+			if (decision == 1) { printNumDisciples(); break; }
+			else if (decision == 2) { cout << "\n Say the number of diciples do you want to change: (ex +3  or -3) "; cin >> numdisciples; setNumDisciples();  break; }
+			else if (decision == 3) { break; }
 			else { break; }
 		case 3:
-			cout << "\nYou want remember your clan name, change clan name or you have wrong? (say remember, change or wrong)";
+			cout << "\nyou want 1-remember your clan name, 2-change clan name or 3-you have wrong?)";
 			cin >> decision;
-			if (strcmp(decision, "remember")) { break; }
-			else if (strcmp(decision, "change")) { break; }
-			else if (strcmp(decision, "wrong")) { break; }
+			if (decision == 1) { printClanName(); break; }
+			else if (decision == 2) {
+				cout << "\n Say the new clan name: ";
+				cin >> newClanName;
+				setClanName(newClanName);
+				strcpy_s(nameClan, newClanName);
+				break; }
+			else if (decision == 3) { break; }
 			else { break; }
-		case 4:
-			cout << "\nYou want change yout capabilities or see or you have wrong? ( say change or see o wrong)";
+		/*case 4:
+			cout << "\nyou want change yout capabilities or see or you have wrong? ( say change or see o wrong)";
 			cin >> decision;
-			if (strcmp(decision, "change")) { break; }
-			else if (strcmp(decision, "see")) { break; }
-			else if (strcmp(decision, "wrong")) { break; }
+			if (decision ==1) { break; }
+			else if (decision == 2) { break; }
+			else if (decision == 3) { break; }
 			else { break; }
 
 		case 5:
-			cout << "\nYou want set your weapons, get number, see if u has the weapon or you have wrong? (say want, get, see or wrong)";
+			cout << "\nyou want set your weapons, get number, see if u has the weapon or you have wrong? (say want, get, see or wrong)";
 			cin >> decision;
-			if (strcmp(decision, "want")) { break; } // de aqui para abajo se organiza falta funciones;
-			else if (strcmp(decision, "get")) { break; }
-			else if (strcmp(decision, "see")) { break; }
-			else if (strcmp(decision, "wrong")) { break; }
+			cin >> decision;
+			if (decision ==1) { break; }
+			else if (decision == 2) { break; }
+			else if (decision == 3) { break; }
 			else { break; }
 		case 6:
-			cout << "\nYou want set your animal, get animals, see if u has the animals or you have wrong? (say want, get, see or wrong)";
+			cout << "\nyou want set your animal, get animals, see if u has the animals or you have wrong? (say want, get, see or wrong)";
+			
 			cin >> decision;
-			if (strcmp(decision, "want")) { break; } // de aqui para abajo se organiza falta funciones;
-			else if (strcmp(decision, "get")) { break; }
-			else if (strcmp(decision, "see")) { break; }
-			else if (strcmp(decision, "wrong")) { break; }
+			if (decision ==1) { break; }
+			else if (decision == 2) { break; }
+			else if (decision == 3) { break; }
 			else { break; }
 		case 7:
-			cout << "\nYou want register in tournament or see if u are register or you have wrong? ( say register or comprobation o wrong)";
+			cout << "\nyou want register in tournament or see if u are register or you have wrong? ( say register or comprobation o wrong)";
 			cin >> decision;
-			if (strcmp(decision, "register")) { break; }
-			else if (strcmp(decision, "comprobation")) { break; }
-			else if (strcmp(decision, "wrong")) { break; }
-			else { break; }
-
+			if (decision ==1) { break; }
+			else if (decision == 2) { break; }
+			else if (decision == 3) { break; }
+			else { break; }*/
 			
 		}
 	};
